c5ad67c2944237551e88354fb5487eac51ae3e51
Hidai Bar-Mor <hidai25@gmail.com>
2026-03-14 00:21:54 +0200

    Updates

--- assets/arrow-left-byusSYsH.js
+++ assets/arrow-left-byusSYsH.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-19cdOe6B.js";/**
+import{c as o}from"./index-DsMLxl1l.js";/**
  * @license lucide-react v0.555.0 - ISC
  *
  * This source code is licensed under the ISC license.
